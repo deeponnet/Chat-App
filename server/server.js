fcc8ca1675fdccf4bfe6260e94437000a4d29d3a
Deepanshu deeponnet <deathaxe66@gmail.com>
2017-12-21 00:38:39 +0530

    added socket.io

--- server/server.js
+++ server/server.js
@@ -1,12 +1,30 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+	console.log('new user connected');
+
+socket.emit('newEmail', {
+	from: 'abc@gmail.com',
+	text: 'hiii There..'
+});
+
+socket.on('createMessage', (message, callback) => {
+	console.log('New message: ', message);
+	callback();
+});
+});
+
+server.listen(port, () => {
 	console.log(`server is up on ${port}`);
 });
