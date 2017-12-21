8de80fa2210bfbc394ee5cf87ec1f1a8d416106d
Deepanshu deeponnet <deathaxe66@gmail.com>
2017-12-21 13:53:01 +0530

    mlab added

--- public/index.js
+++ public/index.js
@@ -4,14 +4,14 @@ var socket = io();
 		console.log('new server connected');
 	});
 
-	
-
 	jQuery('#message-form').on('submit', function(e) {
 		e.preventDefault();
 
 		socket.emit('createMessage', {
-		from: 'user',
-		text: jQuery('[name=message]').val()
+		"lecture" : jQuery('[name=room]').val(),
+		"floor" : jQuery('[name=floor]').val(),
+		"block" : jQuery('[name=block]').val(),
+		"des" : ""
 	}, function() {
 		console.log('got it');
 	});
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
+const MongoClient = require('mongodb').MongoClient;
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -11,18 +12,42 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection', (socket) => {
-	console.log('new user connected');
+MongoClient.connect('mongodb://admin:admin@ds161856.mlab.com:61856/class', (err, db) => {
+	if(err) {
+		console.log(err);
+		return console.log('unable to connect to mongodb server');
+	}
+	console.log('connected to mongodb server');
 
-socket.emit('newEmail', {
-	from: 'abc@gmail.com',
-	text: 'hiii There..'
-});
+	io.on('connection', (socket) => {
+		console.log('new user connected');
 
-socket.on('createMessage', (message, callback) => {
-	console.log('New message: ', message);
-	callback();
-});
+	/*socket.emit('newEmail', {
+		from: 'abc@gmail.com',
+		text: 'hiii There..'
+	}); */
+
+	socket.on('createMessage', (message, callback) => {
+		console.log('New message: ', message);
+		db.collection('lecturerooom').insertOne(
+		{
+		    "lecture" : {
+		        "floor" : message.floor,
+		        "block" : message.block,
+		        "des" : message.des
+		    }
+			}, (err, result) => {
+				if(err) {
+					return console.log('unable to insert ToDo', err);
+				}
+
+				console.log(JSON.stringify(result.ops, undefined, 2));
+			});
+		callback();
+	});
+	});
+
+	
 });
 
 server.listen(port, () => {
